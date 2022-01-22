92bf0d8a3548c96d0b737eefb6f8ad1b1d0c4169
alyaathirah <62088501+alyaathirah@users.noreply.github.com>
2022-01-22 21:36:10 +0800

    Added navigation and fragments

--- RecipeApp/app/src/main/java/com/example/recipeapp/HomeActivity.java
+++ RecipeApp/app/src/main/java/com/example/recipeapp/HomeActivity.java
@@ -1,37 +1,45 @@
 package com.example.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class HomeActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        LinearLayout starLayout = (LinearLayout) findViewById(R.id.star_layout);
-        List<ImageView> star = new ArrayList<>();
-        starLayout.setBackgroundColor(Color.TRANSPARENT);
-        for(int i=0; i<5; i++){
-            star.add(new ImageView(HomeActivity.this));
-            star.get(i).setImageResource(R.drawable.ic_star);
-            starLayout.addView(star.get(i));
-        }
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navController = Navigation.findNavController(this, R.id.fragment);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+
+//        LinearLayout starLayout = (LinearLayout) findViewById(R.id.star_layout);
+//        List<ImageView> star = new ArrayList<>();
+//        starLayout.setBackgroundColor(Color.TRANSPARENT);
+//        for(int i=0; i<5; i++){
+//            star.add(new ImageView(HomeActivity.this));
+//            star.get(i).setImageResource(R.drawable.ic_star);
+//            starLayout.addView(star.get(i));
+//        }
 
 
     }
